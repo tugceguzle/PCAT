5a9ddc0a7a71a4924dbc90f7e2d997070815a58d
Tugce Guzle <tugce.guzle1928@gmail.com>
2023-04-06 15:32:25 +0300

    add files

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/photos/edit/:id', pageController.getEditPage);
 
  
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
