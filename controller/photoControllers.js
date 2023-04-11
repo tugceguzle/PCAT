190b6d94b61e2e98aaa7bd5ff039fd8bb16091b3
Tugce Guzle <tugce.guzle1928@gmail.com>
2023-04-12 00:26:19 +0300

    update delete image

--- controller/photoControllers.js
+++ controller/photoControllers.js
@@ -53,8 +53,8 @@ exports.updatePhoto = async (req, res) => {
 
 exports.deletePhoto = async (req, res) => {
   const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/../public' + photo.image;
-  fs.unlinkSync(deletedImage);
+  // let deletedImage = __dirname + '/../public' + photo.image;
+  // fs.unlinkSync(deletedImage);
   await Photo.findByIdAndRemove(req.params.id);
   res.redirect('/');
 };
